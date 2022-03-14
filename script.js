4dcae82b5f02feac3ef008ef800896109c2269fa
Chaithu1216 <99240181+Chaithu1216@users.noreply.github.com>
2022-03-14 19:28:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var s="skill safari"
+console.log("The length of string is " +s.length)
+console.log("The string in uppercase: " +s.toUpperCase())
+console.log("First occurrence of a in string is at the index " +s.indexOf("a"))
+console.log("Second occurrence of a in string is at the index " +s.indexOf("a",8))
+console.log("Captain America:This isn't going to change what happened. \nIron Man: I don't care. He killed my mom.")
+
+
+var height=2 //in meters
+var weight=70 //in kgs
+BMI=weight/(height*height)
+console.log("The BMI based on height & weight is " +BMI) 
